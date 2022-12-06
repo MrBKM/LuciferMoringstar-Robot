6907b0a994096f30bbbb0078ebcb694014f17098
BKM TG <119480777+MrBKM@users.noreply.github.com>
2022-12-06 18:00:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ def is_enabled(value, default):
 
 
 # Creator
-CREATOR_NAME = os.environ.get("CREATOR_NAME", "Bikash")
-CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "Bikash_9999")
+CREATOR_NAME = os.environ.get("CREATOR_NAME", "BIKASH")
+CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "BIKASH_9999")
 
 # Account
 API_HASH = os.environ.get("API_HASH", "8719d11809492f836004a39b42599215")
 API_ID = os.environ.get("API_ID", "12694006")
 # About Bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5567354112:AAH3f9bi60I8HiZ5mCu38-mBeMCkdd62T1I")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 PICS = os.environ.get("PICS", "https://telegra.ph/file/913f9af3d11d8c0306043.jpg")
 # Database
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Balmiki")
@@ -28,9 +28,9 @@ DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Balmiki:Balmiki@clu
 ADMINS = os.environ.get("ADMINS", "953377581 5630723610")
 SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Tk_movies_adda")
 AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001506877410")
-CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001822275183").split()]
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001646581413")
-GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001576725431")
+CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001895439828").split()]
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001839468546")
+GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001886092713")
 FILTER_DEL_SECOND = int(os.environ.get("FILTER_DEL_SECOND", "600"))
 
 # AutoFilter
